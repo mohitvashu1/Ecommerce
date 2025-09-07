aab43b07387d075584c1c56b126b43eb2419fbc4
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-09-07 23:36:04 +0530

    update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,13 +19,13 @@ const Contact = () => {
         <div className="flex flex-col justify-center items-start gap-6">
           <p className="font-semibold text-xl text-gray-600">Our Store</p>
           <p className="text-gray-500">
-            54709 willms Station <br /> Suite 350, Washington, USA
+            54709  Station <br />  350, Patna
           </p>
           <p className="text-gray-500">
-            Tel: (415) 555-0132 <br /> Email: admin@forever.com
+            Tel: +91 748550132 <br /> Email: admin@mohit.com
           </p>
           <p className="font-semibold text-xl text-gray-600">
-            Careers at Forever
+            Careers at URABN
           </p>
           <p className="text-gray-500">
             Learn more about our teams and job openings.
